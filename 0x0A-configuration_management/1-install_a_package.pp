4dafa78e68cc68d8121f6ea5ada1f927125adaf4
ikeali <ikechukwualisigwe01@mail.com>
2024-03-25 09:33:21 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Puppet Manifest for installing puppet-lint version 2.5.0
+# install puppet-lint -v 2.5.0
 
-exec { 'install_puppet_lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0\n',
-}
-
-file { '/etc/puppetlabs/puppet/puppet.conf':
-  ensure  => file,
-  content => "[main]\nlogdir=/var/log/puppet\nvardir=/var/lib/puppet\nssldir=/var/lib/puppet/ssl\ncodedir=/etc/puppetlabs/code\n\n[agent]\nserver=puppet\npluginsync=true\nreport=true\nenvironment=production\n",
-}
-
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+  }

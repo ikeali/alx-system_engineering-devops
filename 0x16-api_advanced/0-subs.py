6529fc1ac29b81af7725b2a2a79a50fa5e031216
ikeali <ikechukwualisigwe01@mail.com>
2024-05-08 14:33:15 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""This module makes a request to the reddit api"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """gets number of subscribers of a subreddit"""
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'My User Agent 1.0',
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    if subreddit is None:
+        return (0)
+    try:
+        res = requests.get('https://www.reddit.com/r/'+subreddit
+                           + '/about.json', headers=headers,
+                           allow_redirects=False)
+        subreddit_data = res.json()
+    except Exception:
+        return (0)
+    if 'error' in subreddit_data.keys():
+        return (0)
+    return(subreddit_data['data']['subscribers'])

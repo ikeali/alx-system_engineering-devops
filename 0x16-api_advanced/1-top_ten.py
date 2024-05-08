1a47a8121058bfc4b36da2d2c1e49704c276f234
ikeali <ikechukwualisigwe01@mail.com>
2024-05-08 14:30:13 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""This module makes a request to the reddit api"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """gets number of subscribers of a subreddit"""
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'My User Agent 1.0',
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    try:
+        res = requests.get('https://www.reddit.com/r/'+subreddit
+                           + '/hot.json?limit=10', headers=headers,
+                           allow_redirects=False)
+        subreddit_data = res.json()
+    except Exception:
+        print('None')
+    if 'error' in subreddit_data.keys():
+        print('None')
+    else:
+        details = subreddit_data['data']['children']
+        for detail in details:
+            print(detail['data']['title'])

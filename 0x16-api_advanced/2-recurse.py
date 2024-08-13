1f12da968e2b097c8ad7e6eb9710dcc3ca34abc4
ikeali <ikechukwualisigwe01@gmail.com>
2024-08-13 12:55:20 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""This module makes a request to the reddit api"""
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    """recursively calls the reddit api to fetch all hot articles"""
-    headers = {
-            'User-Agent': 'My User Agent 1.0',
-    }
-    try:
-        if after is None:
-            return hot_list
-        if after == '':
-            res = requests.get('https://www.reddit.com/r/'+subreddit
-                               + '/hot.json', headers=headers,
-                               allow_redirects=False)
-        else:
-            res = requests.get('https://www.reddit.com/r/'+subreddit
-                               + '/hot.json?after={}'.format(after),
-                               headers=headers, allow_redirects=False)
-        subreddit_data = res.json()
-    except Exception:
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
         return (None)
-    if 'error' in subreddit_data.keys():
-        return (None)
-    details = subreddit_data['data']['children']
-    for detail in details:
-        hot_list.append(detail['data']['title'])
-    after = subreddit_data['data']['after']
-    return (recurse(subreddit, hot_list, after))
-
